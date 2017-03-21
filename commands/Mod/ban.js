be819e537713e35d8ca66b66942d939ed08d4f85
bdistin <bdistin@gmail.com>
2017-03-21 16:57:39 -0500

    Repeated Code

--- commands/Mod/ban.js
+++ commands/Mod/ban.js
@@ -5,34 +5,26 @@ exports.run = (client, msg, [user, ...reason]) => {
 
 	if (!confs.logChannel) return msg.channel.sendMessage('You must set the logChannel conf to use this command.');
 
-	if (typeof user === 'string') {
-		if (/^<@!?\d+>$/.test(user)) user = /\d+/.exec(user)[0];
-
-		const embed = new RichEmbed()
+	const embed = new RichEmbed()
 			.setColor(200 * 256 * 256)
-			.setAuthor(`[${user}]`)
 			.setTimestamp()
 			.setFooter('Ban', client.user.avatarURL)
 			.addField('__**Responsible Mod**__', `${msg.author.username} #${msg.author.discriminator}`)
 			.addField('__**Reason**__', `${reason.join(' ')}\n\u200b`);
+
+	if (typeof user === 'string') {
+		if (/^<@!?\d+>$/.test(user)) user = /\d+/.exec(user)[0];
+		embed.setAuthor(`[${user}]`);
 		return msg.guild.ban(user)
 			.then(() => client.channels.get(confs.logChannel).sendEmbed(embed))
 			.catch(err => msg.reply(`There was an error trying to ban ${user.username}: ${err}`));
-	} else {
-		const embed = new RichEmbed()
-			.setColor(200 * 256 * 256)
-			.setAuthor(`${user.username} #${user.discriminator} [${user.id}]`, user.avatarURL)
-			.setTimestamp()
-			.setFooter('Ban', client.user.avatarURL)
-			.addField('__**Responsible Mod**__', `${msg.author.username} #${msg.author.discriminator}`)
-			.addField('__**Reason**__', `${reason.join(' ')}\n\u200b`);
-		if (!msg.guild.member(user).roles.exists('name', confs.modRole) && !msg.guild.member(user).roles.exists('name', confs.adminRole) && user.id !== client.user.id) {
-			return msg.guild.member(user).ban()
+	} else if (!msg.guild.member(user).roles.exists('name', confs.modRole) && !msg.guild.member(user).roles.exists('name', confs.adminRole) && user.id !== client.user.id) {
+		embed.setAuthor(`${user.username} #${user.discriminator} [${user.id}]`, user.avatarURL);
+		return msg.guild.member(user).ban()
 				.then(() => client.channels.get(confs.logChannel).sendEmbed(embed))
 				.catch(err => msg.reply(`There was an error trying to ban ${user.username}: ${err}`));
-		} else {
-			return msg.channel.sendMessage('Say What?!?');
-		}
+	} else {
+		return msg.channel.sendMessage('Say What?!?');
 	}
 };
 
