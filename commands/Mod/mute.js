c71a3ce587b11fc84eb38de543b5dd1da4d908b4
bdistin <bdistin@gmail.com>
2017-03-21 17:03:58 -0500

    No need to .then() here

--- commands/Mod/mute.js
+++ commands/Mod/mute.js
@@ -15,13 +15,11 @@ exports.run = (client, msg, [user, ...reason]) => {
 	if (!msg.guild.member(user).roles.exists('name', confs.modRole) && !msg.guild.member(user).roles.exists('name', confs.adminRole) && user.id !== client.user.id) {
 		return msg.guild.member(user).addRole(msg.guild.roles.find('name', 'mute'))
 			.then(() => {
-				client.channels.get(confs.logChannel).sendEmbed(embed)
-					.then(() => {
-						client.setTimeout(() => {
-							msg.guild.member(user).removeRole(msg.guild.roles.find('name', 'mute'))
-								.catch(err => msg.reply(`There was an error trying to un-mute ${user.username}: ${err}`));
-						}, 600000);
-					});
+				client.channels.get(confs.logChannel).sendEmbed(embed);
+				client.setTimeout(() => {
+					msg.guild.member(user).removeRole(msg.guild.roles.find('name', 'mute'))
+					.catch(err => msg.reply(`There was an error trying to un-mute ${user.username}: ${err}`));
+				}, 600000);
 			})
 			.catch(err => msg.reply(`There was an error trying to mute ${user.username}: ${err}`));
 	} else {
